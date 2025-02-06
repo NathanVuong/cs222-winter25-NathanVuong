02ce44ddab2478f250d740e79650466febce1524
ncvuong <vuongnathan3@gmail.com>
2025-02-05 19:12:48 -0800

    Debugging

--- test/utils/general_test_utils.h
+++ test/utils/general_test_utils.h
@@ -173,7 +173,7 @@ namespace PeterDBTesting {
         std::string res;
         for (auto const &pair: inMap)
             res += pair.first + ": " + pair.second + ", ";
-        res.substr(res.find_last_of(", "));
+        res = res.substr(res.find_last_of(", "));
         return trim_copy(res.substr(0, res.find_last_of(", ") - 1));
     }
 
